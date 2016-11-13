bdda1226e2560e4b92d7509f9cb0f4e0d848999a
Ioannis Kokkinis <info@upg.gr>
2016-11-14 00:00:46 +0200

    Updated assets.phpconfig.php .

---
+++ assets.php
@@ -0,0 +1,7 @@
+<?php include('config.php')
+
+foreach ($cats as $val)
+{
+  echo $val.'<br />';
+}
+?>
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+//folders for assets
+$newsfolder = 'news'; //10-30 minutes
+$adsfolder = 'ads'; //1-5 minutes
+$promofolder = 'promo'; //1-3 minutes
+$fillinisfolder = 'fillins'; //ideally 1,2,3 minute clips
+$moviesfolder = 'movies'; // 1.5 hours + movies
+$episodesfolder = 'episodes'; //20-40 minutes
+$kidsmoviesfolder = 'movies-kids'; //60+ minutes
+$kidsshortsfolder = 'shorts-kids'; //2-6 minutes
+$documentariesfolder = 'documentary'; //60 to 80 minutes
+$musicclipsfolder = 'music-clips'; //2-8 minute clips
+$shortmoviesfolder = 'movies-short'; //20-100 minute movies
+$telemarketing = 'telemarketing'; //1-4 hours
+$sports = 'sports'; //30 - 120 minutes
+$shortdocumentariesfolder = 'documentaries-short'; //20-60 minutes
+
+$cats = array($newsfolder,$adsfolder,$promofolder,$fillinisfolder,$moviesfolder,$episodesfolder,$kidsmoviesfolder,$kidsshortsfolder,$documentariesfolder,$musicclipsfolder,$shortmoviesfolder,$telemarketing,$sports,$shortdocumentariesfolder);
+ ?>
